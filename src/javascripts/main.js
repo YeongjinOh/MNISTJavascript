3dc935a7c3e20a2678537b6a3d968607c13a2ebf
yeongjinoh <oyj9109@snu.ac.kr>
2016-11-15 04:11:36 +0900

    define basic digit module

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,30 +1,48 @@
-var simplesCount = 50;
+/** amba **/
+
+var tag = function (name) {
+    return new Tag(name);
+};
+var Tag = function (name) {
+    this.$ = $('<' + name + '>');
+    this.$.data('tag', this);
+};
+Tag.prototype.appendTo = function (parent) {
+    this.$.appendTo(parent.$);
+    return this;
+};
+Tag.prototype.append = function () {
+    this.$.appendTo($('body'));
+    return this;
+};
+
+/** initial setting **/
 var size = 28;
 
-var cv = tag('canvas').append();
+function digit (row, col) {
+    var cv = tag('canvas');
+    var canvas = cv.$.get(0);
+    canvas.height = size;
+    canvas.width = size;
+    var ctx = canvas.getContext("2d");
+    var canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var digits = mnist[col].range(row, row);
+    var digit = digits[0];
+    for (var i = 0; i < digit.length; i++) {
+        var pointColor = digit[i] * 255;
+        var position = Math.floor(i/size)*canvas.width + (i % size);
+        canvasData.data[position * 4 + 0] = 255-pointColor;
+        canvasData.data[position * 4 + 1] = 255-pointColor;
+        canvasData.data[position * 4 + 2] = 255-pointColor;
+        canvasData.data[position * 4 + 3] = 255;
+    }
+    ctx.putImageData(canvasData, 0, 0);
+    return cv;
+}
 
-// var canvas = document.getElementById("mnistCanvas");
-var canvas = cv.$.get(0);
-canvas.height = size*simplesCount;
-var ctx = canvas.getContext("2d");
-
-var canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-var colOffset, rowOffcet;
-console.log(canvasData.data);
-for (var col=0;col < 10; col++) {
-    colOffset = (9-col)*size;
-    var digits = mnist[col].range(0, simplesCount-1);
-    for (var row=0;row < simplesCount; row++) {
-        var digit = digits[row];
-        rowOffcet = row*size;
-        for (var i = 0; i < digit.length; i++) {
-            var pointColor = digit[i] * 255;
-            var position = (colOffset+(rowOffcet + Math.floor(i/size))*canvas.width) + (i % size);
-            canvasData.data[position * 4 + 0] = 255-pointColor;
-            canvasData.data[position * 4 + 1] = 255-pointColor;
-            canvasData.data[position * 4 + 2] = 255-pointColor;
-            canvasData.data[position * 4 + 3] = 255;
-        }
+for (var i=0; i<100; i++) {
+    var row = tag('div').append();
+    for (var j=0; j<10; j++) {
+        digit(i,j).appendTo(row);
     }
 }
-ctx.putImageData(canvasData, 0, 0);
