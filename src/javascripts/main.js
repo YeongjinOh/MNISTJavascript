a5dcd12e00ba4612a30e347e3fb41da27083c693
yeongjinoh <oyj9109@snu.ac.kr>
2016-11-15 20:21:44 +0900

    basic utils for ML

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -15,19 +15,25 @@ Tag.prototype.append = function () {
     this.$.appendTo($('body'));
     return this;
 };
+Tag.prototype.remove = function () {
+    this.$.remove();
+    return this;
+};
 
 /** initial setting **/
-var size = 28;
 
-function digit (row, col) {
+// get mnist digit
+function mn(row, col) {
+    return mnist[col].range(row, row)[0];
+}
+function showDigit (row, col) {
     var cv = tag('canvas');
     var canvas = cv.$.get(0);
     canvas.height = size;
     canvas.width = size;
     var ctx = canvas.getContext("2d");
     var canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    var digits = mnist[col].range(row, row);
-    var digit = digits[0];
+    var digit = mn(row, col);
     for (var i = 0; i < digit.length; i++) {
         var pointColor = digit[i] * 255;
         var position = Math.floor(i/size)*canvas.width + (i % size);
@@ -40,9 +46,130 @@ function digit (row, col) {
     return cv;
 }
 
-for (var i=0; i<100; i++) {
-    var row = tag('div').append();
-    for (var j=0; j<10; j++) {
-        digit(i,j).appendTo(row);
+/** show digits **/
+
+// var prev = tag('div');
+// for (var i=0; i<10; i++) {
+//     (function (i) {
+//         setTimeout(function () {
+//             var row = tag('div');
+//             for (var j = 0; j < 10; j++) {
+//                 showDigit(i, j).appendTo(row);
+//             }
+//             prev.remove();
+//             row.append();
+//             prev = row;
+//         }, i * 100)
+//     })(i);
+// }
+
+/** basic utils **/
+
+function sigmoid(z) {
+    return 1.0/(1.0+Math.exp(-z));
+}
+
+function sigmoid_prime(z) {
+    return sigmoid(z)*(1-sigmoid(z));
+}
+
+function softmax (y) {
+    var res = new Array(10);
+    var sum = 0;
+    for (var i=0; i<10; i++) {
+        res[i] = Math.exp(y[i]);
+        sum += res[i];
+    }
+    for (i=0; i<10; i++) {
+        res[i] = res[i]/sum;
+    }
+    return res;
+}
+
+
+/** initialize model **/
+
+// fully connected
+function getW (n, k) {
+    var w = new Array(k);
+    // random initialize
+    for (var i=0; i<k; i++) {
+        w[i] = new Array (n);
+        for (var j=0; j<n; j++) {
+            w[i][j] = Math.random();
+        }
+    }
+    return w;
+}
+function getB (k) {
+    var b = new Array(k);
+    for (var i=0; i<k; i++) {
+        b[i] = Math.random();
+    }
+    return b;
+}
+
+// predict
+function getSolution (cur) {
+    var sol = new Array(10);
+    for (var i=0; i<10; i++) {
+        sol[i] = 0;
+    }
+    sol[cur] = 1;
+    return sol;
+}
+// from given x and w, return y
+function model (x, w, b) {
+    var y = new Array(10);
+    for (i=0; i<w.length; i++) {
+        y[i] = b[i];
+        for (j=0; j<w[i].length; j++) {
+            y[i] += w[i][j] * x[j];
+        }
+    }
+    return softmax(y);
+}
+
+function train (row, col, w, b) {
+    var x = mn(row,col), i, j;
+    var sol = getSolution(col);
+    var y = model(x, w, b);
+    for (i=0; i<w.length; i++) {
+        for (j=0; j<w[i].length; j++) {
+            // -= ? += ?
+            w[i][j] -= alpha*x[j]*(sol[i]-y[i]);
+        }
     }
 }
+
+/** default parameters **/
+var size = 28, k = 10;
+var alpha = 0.01; // learning rate
+var i,j;
+var w1 = getW(size*size,k);
+var b1 = getB(k);
+
+
+var answer = 1;
+var trainNum = 700;
+
+for (i=0; i<10; i++) {
+    for (j=0; j<trainNum; j++) {
+        train(j, i, w1, b1);
+    }
+}
+var x = mn(trainNum+1, answer);
+var y = model(x, w1, b1);
+
+function maxIdx (y) {
+    var max = -1, idx = -1;
+    for (var i=0; i<y.length; i++) {
+        if (max < y[i]) {
+            max = y[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+alert(maxIdx(y) + ' with ' + y[maxIdx(y)] + ' where answer is ' + answer);
+console.log(y);
